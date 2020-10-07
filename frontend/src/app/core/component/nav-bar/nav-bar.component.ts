03abcb3d3563e030939f5f80056492931b8595b3
gilsonmneto <gilsonmneto@yahoo.com.br>
2020-10-07 02:34:55 -0300

    small changes

--- frontend/src/app/core/component/nav-bar/nav-bar.component.ts
+++ frontend/src/app/core/component/nav-bar/nav-bar.component.ts
@@ -1,9 +1,7 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-    selector: 'app-nav-bar',
-    templateUrl: './nav-bar.component.html'
+  selector: "app-nav-bar",
+  templateUrl: "./nav-bar.component.html",
 })
-export class NavBarComponent { 
-
-}
+export class NavBarComponent {}
